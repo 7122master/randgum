4124658ca2af6200d36b9c36717bf7876c0b8861
OmeletWithoutEgg <50587059+OmeletWithoutEgg@users.noreply.github.com>
2020-06-15 20:38:53 +0800

    add wordfiles and adjust weighting method

--- jielong.py
+++ jielong.py
@@ -1,15 +1,18 @@
 from pypinyin import lazy_pinyin, Style, pinyin
 
-f = open('chinese_word.txt', 'r', encoding='UTF-8')
-
-words = [line.rstrip('\n') for line in f.readlines()]
+words = []
+wordfiles = ['chinese_word.txt', 'chinese_poetry.txt', 'names.txt']
+for filename in wordfiles:
+    f = open(filename, 'r', encoding='UTF-8')
+    words += [line.rstrip('\n') for line in f.readlines()]
+    f.close()
 
 # for word in words:
-#     print(lazy_pinyin(word, style=Style.NORMAL))
+#     print(lazy_pinyin(word, style=Style.TONE3))
 
 def match(a, b):
-    a = pinyin(a, style=Style.NORMAL, heteronym=True)
-    b = pinyin(b, style=Style.NORMAL, heteronym=True)
+    a = pinyin(a, style=Style.TONE3, heteronym=True)
+    b = pinyin(b, style=Style.TONE3, heteronym=True)
     n = min(len(a), len(b))
     res = 0
     for i in range(n, 0, -1):
@@ -18,12 +21,12 @@ def match(a, b):
         same = True
         weight = 0
         for j in range(i):
-            w = len(set(sa[j]) & set(sb[j]))
+            w = len(set(sa[j][0:2]) & set(sb[j][0:2]))
             if w == 0:
                 same = False
             weight += w 
         if same:
-            res = max(res, weight * 10 + len(b))
+            res = max(res, weight * 100 + min(5, len(b)))
     return res
 
 # print(match('沉默', '沉默是金'))
@@ -31,7 +34,7 @@ def match(a, b):
 try:
     while True:
         S = input()
-        print(pinyin(S, style=Style.NORMAL, heteronym=True))
+        print(pinyin(S, style=Style.TONE3, heteronym=True))
         results = [(match(S, word), word) for word in words]
         print(sorted(results, reverse=True)[0:100])
 except EOFError:

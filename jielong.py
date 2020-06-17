6832c470a5cb57ca891c53931882ef72308e235f
OmeletWithoutEgg <50587059+OmeletWithoutEgg@users.noreply.github.com>
2020-06-17 11:01:18 +0800

    removed multiple syllable decision and rewrite to be faster

--- jielong.py
+++ jielong.py
@@ -1,7 +1,7 @@
-from pypinyin import lazy_pinyin, Style, pinyin
+from pypinyin import Style, pinyin, lazy_pinyin
 
 words = []
-wordfiles = [('chinese_word.txt', 1), ('chinese_poetry.txt', 0.7), ('names.txt', 10)]
+wordfiles = [('chinese_word.txt', 1), ('chinese_poetry.txt', 0.8), ('names.txt', 10)]
 for filename, weight in wordfiles:
     f = open(filename, 'r', encoding='UTF-8')
     words += [(line.rstrip('\n'), weight) for line in f.readlines()]
@@ -10,32 +10,32 @@ for filename, weight in wordfiles:
 # for word in words:
 #     print(lazy_pinyin(word, style=Style.TONE3))
 
-def match(a, b):
-    a = pinyin(a, style=Style.TONE3, heteronym=True)
-    b = pinyin(b, style=Style.TONE3, heteronym=True)
-    n = min(len(a), len(b))
-    res = 0
-    for i in range(n, 0, -1):
-        sa = a[len(a)-i:len(a)]
-        sb = b[0:i]
-        same = True
-        weight = 0
-        for j in range(i):
-            w = len(set(sa[j][0:2]) & set(sb[j][0:2]))
-            if w == 0:
-                same = False
-            weight += w 
-        if same:
-            res = max(res, weight * 100 + min(5, len(b)))
+def build(words):
+    res = {}
+    for word, weight in words:
+        for style, ratio in [(Style.TONE3, weight), (Style.NORMAL, weight * 0.8)]:
+            p = lazy_pinyin(word, style=style)
+            for i in range(1, len(p)+1):
+                res.setdefault(tuple(p[0:i]), []).append((ratio * (i + min(5, len(word))), word))
     return res
+
+def search(s, mp):
+    candidate = []
+    for style in [Style.TONE3, Style.NORMAL]:
+        candidate += mp.setdefault(tuple(lazy_pinyin(s, style=style)), [])
+    return sorted(candidate, reverse=True)[0:100]
 
 # print(match('沉默', '沉默是金'))
 
+W = build(words)
+print('Build Completed')
 try:
     while True:
         S = input()
-        print(pinyin(S, style=Style.TONE3, heteronym=True))
-        results = [(match(S, word) * weight, word) for word, weight in words]
-        print(sorted(results, reverse=True)[0:100])
+        print(lazy_pinyin(S, style=Style.TONE3))
+        res = []
+        for i in range(len(S)):
+            res.append(search(S[i:], W))
+        print(sorted(res, reverse=True)[0:100])
 except EOFError:
     pass

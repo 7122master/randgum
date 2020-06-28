9b951b920bfb4880cdac3dd916e24afef77151b8
xSeanliux <zhxnliu@gmail.com>
2020-06-28 11:51:31 +0800

    More support for GPU

--- config/config.py
+++ config/config.py
@@ -1,7 +1,19 @@
 import torch
 # Define hyper parameter
 use_cuda = True if torch.cuda.is_available() else False
-device = 'cpu'
+
+# setting device on GPU if available, else CPU
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('Using device:', device)
+print()
+
+#Additional Info when using cuda
+if device.type == 'cuda':
+    print(torch.cuda.get_device_name(0))
+    print('Memory Usage:')
+    print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
+    print('Cached:   ', round(torch.cuda.memory_cached(0)/1024**3,1), 'GB')
+
 # for dataset
 dataset_paths = [
 	'./data/chinese_word.txt',

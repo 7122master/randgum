5354c670457ead763283cb4cad0b6e28fa53fa4b
OmeletWithoutEgg <50587059+OmeletWithoutEgg@users.noreply.github.com>
2020-07-15 15:43:05 +0800

    solved 'blocked by cors policy'

--- server.py
+++ server.py
@@ -3,28 +3,27 @@ from flask import Flask, request
 import jielong.jielong as jielong
 import zhuyin.evaluate as zhuyin
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='')
 
 JL = jielong.Solver()
 print('jielong build complete')
 print('loading zhuyin...')
 ZY = zhuyin.build()
 print('zhuyin load complete')
-# ZY.evaluate(testing_word)
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
 
 @app.route('/jielong')
 def jielong():
-    query = request.args.get('query')
+    query = request.args.get('name')
     return { "result": JL.solve(query) }
 
 @app.route('/zhuyin')
 def zhuyin():
-    query = request.args.get('query')
+    query = request.args.get('name')
     return { "result": ZY.evaluate(query) }
-
-# @app.route('/')
-# def root():
-#     return app.send_static_file('index.html')
 
 if __name__ == "__main__":
     app.run()
